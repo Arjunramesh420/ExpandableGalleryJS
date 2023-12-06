e4a07f7879caee40190a799f4904c510a127ce1f
ArjunRamesh <arjunramesh2001@gmail.com>
2023-12-06 12:54:27 +0530

    Expansible Image Gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import infras from './path/to/infras-image.jpg';
 
-function App() {
+const App = () => {
+  function togglePanel(panel) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach((p) => {
+      if (p !== panel) {
+        p.classList.remove('active');
+      }
+    });
+
+    panel.classList.toggle('active');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="container3">
+        <div
+          className="panel"
+          style={{ backgroundImage: `url()` }}
+          onClick={(event) => togglePanel(event.target)}
         >
-          Learn React
-        </a>
-      </header>
+          <h4>Fire</h4>
+        </div>
+
+        <div
+          className="panel"
+          style={{ backgroundImage: `url()` }}
+          onClick={(event) => togglePanel(event.target)}
+        >
+          <h4>Water</h4>
+        </div>
+        
+        <div
+          className="panel"
+          style={{ backgroundImage: `url()` }}
+          onClick={(event) => togglePanel(event.target)}
+        >
+          <h4>Wind</h4>
+        </div>
+        
+        <div
+          className="panel"
+          style={{ backgroundImage: `url()` }}
+          onClick={(event) => togglePanel(event.target)}
+        >
+          <h4>Earth</h4>
+        </div>
+        
+        <div
+          className="panel"
+          style={{ backgroundImage: `url()` }}
+          onClick={(event) => togglePanel(event.target)}
+        >
+          <h4>Space</h4>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
